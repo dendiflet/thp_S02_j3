5597a529f8619b600034b5e4190856ea422787b0
dendiflet <bretin.iannis@gmail.com>
2019-01-16 12:28:35 +0100

    authentification bot00 bonne avec test rspec

---
+++ README.md
@@ -0,0 +1,2 @@
+salut, voici mon projet du mercredi de la semaine 2 de thp
+API twitter au programme
--- lib/bot_twitter_00.rb
+++ lib/bot_twitter_00.rb
@@ -1,29 +1,33 @@
 require 'twitter'
   require 'dotenv'
 
+puts "\e[H\e[2J"
+
   Dotenv.load
+
+def login_twitter
+
+#puts ENV															#fonctionne pas sa mere !
+#puts ENV['TWITTER_API_SECRET']
 
 #a coller pour faire du irb a partir de  la !
 #binding.pry
 
-def login_twitter
-	client = Twitter::REST::Client.new do |config|
-	  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]				# ce sont les
-	  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]			# quatres ligne
-	  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]				# de connection
-	  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]		# a tweeter
-	  end
-  return Client
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+puts client
+end
 end
 
-puts Client
-
-#binding.pry
-
+login_twitter
 
 
 # ligne qui permet de tweeter sur ton compte
-#client.update('Mon premier tweet en Ruby !!!!')
+#client.update('Mon premier tweet en Ruby !!!! mais oui mamen !')					#ne fonctionne pas en l'état
+
 
 
 
