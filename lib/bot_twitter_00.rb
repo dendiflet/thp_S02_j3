b28383986b7cd7f65d19abf1d26c790c65c693e0
dendiflet <bretin.iannis@gmail.com>
2019-01-16 13:13:00 +0100

    pause manger

--- lib/bot_twitter_00.rb
+++ lib/bot_twitter_00.rb
@@ -1,29 +1,45 @@
 require 'twitter'
   require 'dotenv'
+    require 'pry'
 
 puts "\e[H\e[2J"
 
   Dotenv.load
+client = []
+
 
 def login_twitter
 
-#puts ENV															#fonctionne pas sa mere !
-#puts ENV['TWITTER_API_SECRET']
+
+#####TEST dotenv
+puts "normalement le hash de mes clefs secretes doit s'afficher en dessous"
+puts "#{ENV}"															#fonctionne pas sa mere !
+puts "ici doit s'afficher seulement une clef :"
+puts ENV['TWITTER_API_SECRET']
+puts " "
+#####END test dotenv
+
+
 
 #a coller pour faire du irb a partir de  la !
 #binding.pry
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-puts client
-end
+
+
+	client = Twitter::REST::Client.new do |config|
+	  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+	  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+	  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+	  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+	puts "ici devrait s'afficher client"
+	puts "#{client}"
+	end
+	return client 
 end
 
-login_twitter
-
+client = login_twitter
+puts "voici #{client}"
+#binding.pry
 
 # ligne qui permet de tweeter sur ton compte
 #client.update('Mon premier tweet en Ruby !!!! mais oui mamen !')					#ne fonctionne pas en l'état
